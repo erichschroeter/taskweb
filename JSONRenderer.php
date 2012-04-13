24702b15212a211a9ece05d1c3bbb117c1a14f0d
Erich Schroeter <erichschroeter@gmail.com>
2012-04-13 12:06:50 -0500

    Implemented basic separation of JSON data into HTML tags for JSONRenderer.

--- JSONRenderer.php
+++ JSONRenderer.php
@@ -3,7 +3,73 @@ include 'Renderer.php';
 
 class JSONRenderer implements Renderer {
 
-    public function render() {
+    /** The JSON file exported from taskwarrior. */
+    private $f;
 
+    function __construct($f) {
+        $this->f = $f;
+    }
+
+    function render() {
+        echo '<div class="tasks">';
+        while (($line = fgets($this->f)) !== FALSE) {
+            // remove whitespace/line endings
+            $line = trim($line);
+            // remove comma if exists
+            if ($line[strlen($line)-1] == ',') {
+                $line = rtrim($line, ',');
+            }
+            $task = json_decode($line);
+            switch (json_last_error()) {
+                case JSON_ERROR_DEPTH:
+                    echo ' - Maximum stack depth exceeded';
+                break;
+                case JSON_ERROR_STATE_MISMATCH:
+                    echo ' - Underflow or the modes mismatch';
+                break;
+                case JSON_ERROR_CTRL_CHAR:
+                    echo ' - Unexpected control character found';
+                break;
+                case JSON_ERROR_SYNTAX:
+                    echo ' - Syntax error, malformed JSON';
+                break;
+                case JSON_ERROR_UTF8:
+                    echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+                break;
+                case JSON_ERROR_NONE:
+                default:
+                break;
+            }
+            echo '<div class="task">';
+            echo '<span class="status">' . $task->{'status'} . '</span>';
+            echo '<span class="description">' . $task->{'description'} . '</span>';
+            echo '<div class="meta">';
+            if (isset($task->{'project'})) {
+                $project = $task->{'project'};
+                echo '<span class="project">' . $project . '</span>';
+            }
+            if (isset($task->{'priority'})) {
+                $priority = $task->{'priority'};
+                echo '<span class="priority">' . $priority . '</span>';
+            }
+            if (isset($task->{'tags'})) {
+                $tags = $task->{'tags'};
+                echo '<div class="tags">';
+                foreach ($tags as $tag) {
+                    echo '<span class="tag">' . $tag . '</span>';
+                }
+                echo '</div>';
+            }
+            if (isset($task->{'annotations'})) {
+                $annotations = $task->{'annotations'};
+                echo '<div class="annotations">';
+                foreach ($annotations as $entry => $annotation) {
+                    echo '<span class="annotation">' . $annotation->{'description'} . '</span>';
+                }
+                echo '</div>';
+            }
+            echo '</div></div>';
+        }
+        echo '</div>';
     }
 }
--- index.php
+++ index.php
@@ -5,23 +5,25 @@
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/tasks.css">
 </head>
+<body>
 <?php
-  $r = null;
-  if (file_exists("tasks.json")) {
+$r = null;
+if (file_exists("tasks.json")) {
     include 'JSONRenderer.php';
     $f = @fopen("tasks.json", "r");
-    $r = new JSONRenderer();
-  } else if (file_exists("tasks.csv")) {
+    $r = new JSONRenderer($f);
+} else if (file_exists("tasks.csv")) {
     include 'CSVRenderer.php';
     $f = @fopen("tasks.csv", "r");
     $r = new CSVRenderer($f);
-  }
+}
 
-  if ($r) {
+if ($r) {
     $r->render();
-  } else {
+} else {
     echo "No tasks file found";
-  }
+}
 
 ?>
+</body>
 </html>
